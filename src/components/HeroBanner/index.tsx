48869592f5efa8bf55865ab1e0837f4005fe8b46
Matheus Davy <matheusdavy17@gmail.com>
2023-06-29 16:20:55 -0300

    FIX: remove animation (heroBanner)

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -9,23 +9,14 @@ import { useEffect, useRef } from "react"
 
 export default function HeroBanner() {
 
-    const text = useRef<HTMLParagraphElement | null>()
-    const title = useRef<HTMLHeadingElement>()
-    const description = useRef<HTMLParagraphElement>()
-
-    useEffect(()=>{
-
-    }, [])
-
     return (
         <BannerContent>
             <div className="container">
-                <Text  data-animate='down'>WEBNARS EXCLUSIVOS</Text>
-                <Title  data-animate='left'>Menos Conversinha,
+                <Text >WEBNARS EXCLUSIVOS</Text>
+                <Title >Menos Conversinha,
                     <br />
-                    <span data-animate='right'>
+                    <span>
                         Mais Conversão
-                        
                             <Image
                                 src={detailText}
                                 alt="details"
@@ -34,7 +25,7 @@ export default function HeroBanner() {
                     </span>
                 </Title>
                 <Separator />
-                <Description  data-animate='up'>Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las no seu negócio</Description>
+                <Description >Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las no seu negócio</Description>
             </div>
         </BannerContent>
     )
