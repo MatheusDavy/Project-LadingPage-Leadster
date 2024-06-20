99971d9092550f75d9936390bab3a1059f6aeb22
MatheusDavy <matheusdavy17@gmail.com>
2024-06-20 15:10:11 -0300

    feat: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,28 +28,37 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-    return (
-      <html lang="pt-BR">
-        <head>
-          <title>Leadster</title>
-          <meta charSet="UTF-8" />
-          <meta name="description" content="Webnars Landster" />
-          <meta name="keywords" content="Webnars, Landster, landing page, marketing, chatboot, geraçao de leads, mídia paga, agências" />
-          <meta name="author" content="Matheus Davy" />
-          <link rel="shortcut icon" href={Favicon.src} type="image/x-icon" />
-          
-        </head>
-        <body className={PlusJakartaSans.className}>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle/>
-            <Header />
-            {children}
-            <Footer />
+  return (
+    <html lang="pt-BR">
+      <head>
+        <title>Leadster</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Webnars Landster" />
+        <meta name="keywords" content="Webnars, Landster, landing page, marketing, chatboot, geraçao de leads, mídia paga, agências" />
+        <meta name="author" content="Matheus Davy" />
+        <link rel="shortcut icon" href={Favicon.src} type="image/x-icon" />
+        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P34C3MPP');</script>
 
-          </ThemeProvider>
-        </body>
-      </html>
+      </head>
+      <body className={PlusJakartaSans.className}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Header />
+          {children}
+          <Footer />
 
-    )
-  
+        </ThemeProvider>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P34C3MPP"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+      </body>
+    </html>
+
+  )
+
 }
