b350faf6fd36163338901d41098bf1b2aa573dce
MatheusDavy <matheusdavy17@gmail.com>
2024-06-20 16:36:23 -0300

    feat: add ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,16 @@ export default function RootLayout({
               gtag('config', 'G-FQ7NDJ16WE');
           `}}
         />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5JV3Q796');
+          `}}
+        />
 
       </head>
       <body className={PlusJakartaSans.className}>
@@ -76,7 +86,7 @@ export default function RootLayout({
 
         </ThemeProvider>
 
-        <noscript><iframe src=""
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JV3Q796"
           height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
 
       </body>
